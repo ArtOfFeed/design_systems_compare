3b289291a6ad66cde8c42fa62df306fa40106526
Haytham1337 <y.yatskiv04@gmail.com>
2022-03-25 10:53:52 +0300

    Add mui design system

--- src/issue-view/Material/index.js
+++ src/issue-view/Material/index.js
@@ -1,1 +1,26 @@
-export const Material = () => <h2>Material</h2>
+import { Box,Typography } from '@mui/material';
+import IssueHeader from './components/IssueHeader/IssueHeader'; 
+import IssueContent from './components/issueContent';
+import IssueDetails from './components/issueDetails';
+import MenuSelect from './components/MenuSelect';
+
+export const Material = () => {
+    return (
+        <div className="App">
+          <Box margin='0 auto' width='70%'>
+            <IssueHeader/>
+            <Box display='flex' alignItems="center" marginBottom='10px'>
+              <Typography width='55%' sx={{fontSize:25,fontWeight:'600'}}> Test Issue</Typography>
+              <Box width='10%'/>
+              <MenuSelect ></MenuSelect>
+            </Box>
+            <Box display='flex' flexDirection='row'>
+              <IssueContent/>
+              <Box width='10%'>
+              </Box>
+              <IssueDetails/>
+            </Box>
+          </Box>
+        </div>
+         )
+    }
