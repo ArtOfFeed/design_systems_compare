88a5ef3a6ba062040b81e6d31cf77e0984e5a6a3
ArtOfFeed <zekavovk@yandex.ru>
2022-03-29 23:26:15 +0300

    Revert "Carbon design sistem"

--- src/issue-view/App.js
+++ src/issue-view/App.js
@@ -7,7 +7,6 @@ import {Material} from './Material';
 import {Chakra} from './Chakra';
 import {Braid} from './Braid';
 import './App.css';
-import { CarbonDesignNavigation } from "./Carbon";
 
 const App = () => {
   return (
@@ -22,7 +21,6 @@ const App = () => {
             <NavLink to="material">MUI</NavLink>
             <NavLink to="chakra">Chakra UI</NavLink>
             <NavLink to="braid">Braid</NavLink>
-            <NavLink to="carbon">Carbon</NavLink>
           </ul>
         </nav>
         <Routes>
@@ -34,7 +32,6 @@ const App = () => {
             <Route path={'/chakra'} exact element={<Chakra />} />
             <Route path={'/braid'} exact element={<Braid />} />
         </Routes>
-        <CarbonDesignNavigation />
     </BrowserRouter>
   );
 }
--- src/issue-view/Carbon/components/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import {  } from "carbon-components-react";
-import {  } from "@carbon/icons-react";
-import {  } from "carbon-components";
-
-export const Gap = () => <>&nbsp;</>
