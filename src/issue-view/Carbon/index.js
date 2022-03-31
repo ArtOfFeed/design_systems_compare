2baba26fbf47038febe8e5b5fe762ef69e7e46a0
VelikiyYaroslav <VelikiyYaroslavWork@gmail.com>
2022-03-31 15:34:37 +0300

    add stub for better performance

--- src/issue-view/Carbon/index.js
+++ src/issue-view/Carbon/index.js
@@ -1,20 +1,12 @@
-import React, { Suspense } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
-const CarbonDesign = React.lazy(() => import("./CarbonDesign"));
-const CarbonDesignIssueView = React.lazy(() => import("./issueView"));
-const CarbonDesignAutomationView = React.lazy(() => import("./automationView"));
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+// import { CarbonDesignNavigation as Normal__CarbonDesignNavigation } from "./Routing";
 
 export const CarbonDesignNavigation = () => {
+    // return Normal__CarbonDesignNavigation
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path={'/carbon'} element={<CarbonDesign />}  >
-                    <Route path={'issueView'} element={<CarbonDesignIssueView />} />
-                    <Route path={'automationView'} element={<CarbonDesignAutomationView />} />
-                    <Route path="*" element={<Navigate to="/carbon/issueView" />} />
-                </Route>
-            </Routes>
-        </Suspense>
+        <Routes>
+            <Route path={'/carbon'} element={<div>This is a stub for berret performace. <br/> Uncomment CarbonDesignNavigation in code to get this page active </div>} />
+        </Routes>
     )
 }
